e6e8e19aaf945471265710579ccc2462fd99a8b3
satish chakma <satishchakma.gd@gmail.com>
2023-05-04 06:03:20 +0600

    added cors and dynamic route api for server

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const port = 3000;
 
 var cors = require("cors");
 
+app.use(cors());
+
 const myData = require("./data/chefs.json");
 
 app.get("/", (req, res) => {
   res.send(myData);
+});
+
+app.get("/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const id = req.params.id;
+  const foundData = myData.find((item) => item.id === Number(id));
+  res.send(foundData);
+  //   console.log(foundData);
 });
 
 app.listen(port, () => {
